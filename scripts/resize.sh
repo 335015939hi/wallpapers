a1c849bd04ae1d6fa0ca888c2e15620469b963ea
Tony Zhou <tonyzhou2916@gmail.com>
2026-09-01 13:24:27 -0400

    resize.sh: mkdir -p resized/
    Signed-off-by: Tony Zhou <tonyzhou2916@gmail.com>

--- scripts/resize.sh
+++ scripts/resize.sh
@@ -2,6 +2,8 @@
 
 shopt -s nullglob
 
+mkdir -p resized
+
 for a in ./*.png ./*.PNG ./*.jpg ./*.JPG ./*.jpeg ./*.JPEG ./*.weebp ./*.WEBP; do
   echo $a
   magick "$a" -resize '1920x1200^' -gravity center -crop 1920x1200+0+0 +repage "resized/$a"
