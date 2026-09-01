5ba3c2213a70ebd6845118473a9fa241cef90a66
Tony Zhou <tonyzhou2916@gmail.com>
2026-09-01 10:31:10 -0400

    first commit
    Signed-off-by: Tony Zhou <tonyzhou2916@gmail.com>

---
+++ scripts/resize.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+shopt -s nullglob
+
+for a in ./*.png ./*.PNG ./*.jpg ./*.JPG ./*.jpeg ./*.JPEG ./*.weebp ./*.WEBP; do
+  echo $a
+  magick "$a" -resize '1920x1200^' -gravity center -crop 1920x1200+0+0 +repage "resized/$a"
+done
